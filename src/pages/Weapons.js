725a0aa3abd0cb4b7212fae7bad3058f0874c951
Damien Corticchiato <skaradams69@gmail.com>
2019-09-19 23:56:23 +0200

    switch between weapons styles

--- src/pages/Weapons.js
+++ src/pages/Weapons.js
@@ -9,13 +9,34 @@ import axios from 'axios'
 class Weapons extends Component {
   state = {}
   async componentDidMount() {
-    const weapons = await axios.get('https://mhw-db.com/weapons?q={"type": "great-sword"}')
-    const weaponData = this.weaponHierarchy(weapons.data)
+    // const weapons = await axios.get('https://mhw-db.com/weapons?q={"type": "great-sword"}')
+    // const weaponData = this.weaponHierarchy(weapons.data)
+    const weaponTypes = await axios.get('https://mhw-db.com/weapons?p={"type": true}');
+    const uniqueTypes = weaponTypes.data
+      .map(weapon => weapon.type)
+      .reduce((unique, weapon) => unique.includes(weapon) ? unique : [...unique, weapon], [] );
+    const selectedType = uniqueTypes[0];
+    const weapons = await this.getWeapons(selectedType);
 
     this.setState({
-      weapons: weaponData,
-      selectedWeapon: weaponData[0]
+      weapons: weapons,
+      weaponTypes: uniqueTypes,
+      selectedType: selectedType,
+      selectedWeapon: weapons[0]
+    })
+  }
 
+  async getWeapons(type) {
+    const weapons = await axios.get(`https://mhw-db.com/weapons?q={"type": "${ type }"}`)
+    return this.weaponHierarchy(weapons.data)
+  }
+
+  async refreshWeapons(type) {
+    const weapons = await this.getWeapons(type);
+    this.setState({
+      weapons: weapons,
+      selectedType: type,
+      selectedWeapon: weapons[0]
     })
   }
 
@@ -53,7 +74,14 @@ class Weapons extends Component {
 
   render() {
     return (
-      <div className='weapons'>
+      <div className='weapons-page'>
+        <div className="weapon-types">
+          { this.state.weaponTypes && this.state.weaponTypes.map(type => (
+            <div className="weapon-type" onClick={ () => this.refreshWeapons(type) }>
+              { type }
+            </div>
+          )) }
+        </div>
         <div className='weapons-pane'>
           { this.state.weapons && this.state.weapons.map(weapon => (
             <div key={ weapon.id } style={{ marginBottom: '3em' }} >

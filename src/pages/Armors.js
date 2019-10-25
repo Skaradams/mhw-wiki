db2f329cc59777d1211ea3dfa299074ba37b31a0
Damien Corticchiato <skaradams69@gmail.com>
2019-10-25 17:37:58 +0200

    get all armor pieces

--- src/pages/Armors.js
+++ src/pages/Armors.js
@@ -3,9 +3,16 @@ import { Component } from 'react'
 import axios from 'axios'
 
 class Armors extends Component {
-  state = {}
+  RANK_LOW = 'low';
+  RANK_HIGH = 'high';
+
+  state = {
+    rank: this.RANK_LOW
+  }
   async componentDidMount() {
-    const armorSets = await axios.get('https://mhw-db.com/armor/sets');
+    const armorSets = await axios.get(
+      `https://mhw-db.com/armor/sets?q={"rank": "${ this.state.rank }"}`
+    );
 
     this.setState({
       armorSets: armorSets.data,
@@ -15,13 +22,19 @@ class Armors extends Component {
   render() {
     return (
       <div className="armors-page">
-        { this.state.armorSets && this.state.armorSets.map(set => {
-          return(
-            <div key={ set.id }>
-            { set.name }
+        { this.state.armorSets && this.state.armorSets.map(set => (
+            <div className="armor-set" key={ set.id }>
+              { set.name }
+              <div className="armor-pieces">
+                { set.pieces && set.pieces.map(piece => (
+                  <div key={ piece.id }>
+                    { piece.name }
+                  </div>
+                )) }
+              </div>
             </div>
           )
-        }) }
+        )}
       </div>
     )
   }

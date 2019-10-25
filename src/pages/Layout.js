46920b7bf60fcb9a0a6bb284fef05305094b8a1e
Damien Corticchiato <skaradams69@gmail.com>
2019-10-25 17:07:43 +0200

    add logo in side menu

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Router, Link } from 'react-router-dom'
+import logo from "../assets/images/mhw-logo.png";
 
 const Layout = ({ children }) => (
   <div className="layout">
     <div className="side-menu">
+      <img src={ logo } className="main-logo" />
       <Link className="menu-item" to="/weapons">
         Weapons
       </Link>
